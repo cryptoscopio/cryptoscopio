26407e0729523337164387002677ca919bd5c25e
cryptoscopia <cryptoscopia@gmail.com>
2019-06-11 01:39:41 +1000

    Add front-end dialogues for parsing addresses and records
    - Implement views for handling address and record form submissions
    - Switch from AJAX record uploads to using a regular view
    - Add placeholder dialogues for settings and parsing suggested addresses
    - Show results of parsing files (and partially addresses) on the
    front-end

--- scopio/static/base.js
+++ scopio/static/base.js
@@ -1,67 +1,48 @@
-window.addEventListener('load', function(event) {
-	const upload_select = document.getElementById('upload-select');
-	const upload_input = document.getElementById('upload-input');
-	// Upload the selected files
-	upload_input.addEventListener('change', function(e){
-		upload_select.setAttribute('disabled', true);
-		const default_option = upload_select.getElementsByClassName('default-option')[0];
-		function resetDropdown() {
-			upload_select.removeAttribute('disabled');
-			default_option.innerText = 'Upload records...';
+window.addEventListener('load', function(event){
+	// Register open dialogue triggers for clicking on header menu items
+	for(menu_item of document.querySelectorAll('header menu li')){
+		menu_item.addEventListener('click', function(e){
+			document.getElementById(e.target.dataset.target).className = 'open';
+		});
+	}
+	// Open a dialogue that's pending to be opened, if any. Invoked after closing dialogues.
+	function openPending(){
+		let pending = document.querySelector('dialog.pending');
+		if(pending){
+			pending.className = 'open';
 		}
-		function showError(error_message) {
-			// Reset the dropdown and show error (TODO: show error in DOM)
-			resetDropdown();
-			console.log(error_message);
-		}
-		const upload_request = new XMLHttpRequest();
-		const upload_data = new FormData();
-		// Display upload progress as the selected option
-		default_option.innerText = 'Uploading... 0%';
-		upload_request.upload.addEventListener('progress', function(upload_event){
-			if(upload_event.lengthComputable) {
-				const percentage = Math.round(upload_event.loaded / upload_event.total * 100);
-				// Not an ideal way of detecting completion, but listening for "loadend"
-				// on the upload object doesn't work as expected
-				if(percentage == 100)
-					default_option.innerText = 'Processing...';
-				else
-					default_option.innerText = 'Uploading... ' + percentage + '%';
+	}
+	// Register close dialogue triggers for clicking the dialogue's cancel button
+	for(cancel_button of document.querySelectorAll('dialog input[type="button"]')){
+		cancel_button.addEventListener('click', function(e){
+			e.target.closest('dialog').className = '';
+			openPending();
+		});
+	}
+	// Register close dialogue triggers for clicking within the dialogue backdrop
+	for(dialogue of document.querySelectorAll('dialog')){
+		dialogue.addEventListener('click', function(e){
+			if(e.target.tagName == 'DIALOG'){
+				e.target.className = '';
+				openPending();
 			}
 		});
-		upload_request.addEventListener('abort', function(upload_event){
-			showError('The upload was aborted');
-		});
-		upload_request.addEventListener('timeout', function(upload_event){
-			showError('The upload timed out');
-		});
-		upload_request.addEventListener('error', function(upload_event){
-			showError('The upload encountered an error');
-		});
-		upload_request.addEventListener('loadend', function(upload_event){
-			resetDropdown();
-		});
-		upload_request.open('POST', '.?record_type=' + upload_input.recordType, true)
-		// Get CSRF cookie
-		const csrf_token = document.cookie.replace(/(?:(?:^|.*;\s*)csrftoken\s*\=\s*([^;]*).*$)|^.*$/, "$1");
-		upload_request.setRequestHeader('X-CSRFToken', csrf_token);
-		for (let i=0; i<this.files.length; i++) {
-			upload_data.append('records', this.files[i]);
-		}
-		
-		upload_request.send(upload_data);
-	});
-	// The listener must be on click for opening the file browse dialogue to be allowed
-	upload_select.addEventListener('click', function(e){
-		if(e.target.value) {
-			upload_input.recordType = e.target.value;
-			upload_input.click();
+	}
+	// Register close dialogue trigger when Escape key is pressed
+	document.addEventListener('keyup', function(e){
+		if(e.key == 'Escape' || e.key == 'Esc'){
+			for(open_dialogue of document.querySelectorAll('dialog.open')){
+				open_dialogue.className = '';
+			}
+			openPending();
 		}
 	});
-	// Reset the select right away, though this breaks accessibility (TODO: rethink this)
-	upload_select.addEventListener('change', function(e){
-		e.target.selectedIndex = 0;
-	});
-	// Enable the upload selection once we're ready to handle it
-	upload_select.removeAttribute('disabled');
+	// Open a dialogue if a matching id is in the current URL hash
+	let url = new URL(window.location);
+	if(url.hash){
+		let dialog = document.querySelector(url.hash);
+		if(dialog && dialog.tagName == 'DIALOG'){
+			dialog.className = 'open';
+		}
+	}
 });
--- scopio/views.py
+++ scopio/views.py
@@ -1,28 +1,137 @@
-from django.http import JsonResponse
+from django import forms
+from django.http import HttpResponseNotAllowed
+from django.shortcuts import redirect
 from django.views.generic import TemplateView
+from django.views.generic.edit import FormView
 
+from .explorers import explorers
 from .models import Record
 from .parsers import parsers
 from .utils import wrap_uploaded_file
 
 
-class HomeView(TemplateView):
-	template_name = 'home.html'
+class ParseAddressForm(forms.Form):
+	blockchain = forms.ChoiceField(
+		choices=((name, explorer.DISPLAY_NAME) for name, explorer in explorers.items()),
+		label='Cryptocurrency',
+	)
+	addresses = forms.CharField(
+		widget=forms.Textarea,
+		label='Addresses (separate multiple addresses across new lines)',
+	)
+
+class ParseAddressView(FormView):
+	form_class = ParseAddressForm
+	
+	def get(self, *args, **kwargs):
+		return HttpResponseNotAllowed(['POST'])
 
 	def post(self, request, *args, **kwargs):
-		response = {'error': '',}
-		try:
-			parser = parsers[request.GET.get('record_type')]
+		form = self.get_form()
+		if form.is_valid():
+			results = {'title': 'Addresses parsed', 'messages': []}
+			explorer = explorers[form.cleaned_data['blockchain']]
+			for address in form.cleaned_data['addresses'].split():
+				# TODO: Validate address first
+				# TODO: Wrap in transaction control
+				explorer.parse_address(address)
+		else:
+			# Since we know what the possible errors are, show a more 
+			# user-friendly message instead of what's in `form.errors`.
+			results = {
+				'title': 'Error parsing addresses',
+				'messages': [{
+					'type': 'error',
+					'text': 'Invalid cryptocurrency selected or no addresses provided',
+				}]
+			}
+		request.session['results'] = results
+		return redirect('home')
+
+
+class UploadRecordsForm(forms.Form):
+	platform = forms.ChoiceField(
+		choices=((name, parser.DISPLAY_NAME) for name, parser in parsers.items()),
+	)
+	records = forms.FileField(
+		widget=forms.ClearableFileInput(attrs={'multiple': True, 'accept': '.csv'}),
+		label='',
+	)
+
+class UploadRecordsView(FormView):
+	form_class = UploadRecordsForm
+	
+	def get(self, *args, **kwargs):
+		return HttpResponseNotAllowed(['POST'])
+
+	def post(self, request, *args, **kwargs):
+		form = self.get_form()
+		if form.is_valid():
+			results = {'title': 'Records parsed', 'messages': []}
+			parsed_files = 0
+			parser = parsers[form.cleaned_data['platform']]
 			for record in request.FILES.getlist('records'):
-				parser.parse_file(wrap_uploaded_file(record))
-		except KeyError:
-			response['error'] = 'Invalid record type specified'
-		return JsonResponse(response)
+				try:
+					# Parse the file using the selected platform parser
+					# TODO: Wrap in transaction control
+					parsed, skipped, failed = \
+						parser.parse_file(wrap_uploaded_file(record))
+					# Construct message to show the user about the outcome
+					message = {
+						'type': 'info',
+						'text': f'Parsed file {record.name}',
+						'notes': [],
+					}
+					if parsed:
+						message['notes'] += [{
+							'type': 'success',
+							'text': f'Parsed {parsed} new records',
+						}]
+					if skipped:
+						message['notes'] += [{
+							'type': 'warning',
+							'text': f'Skipped {skipped} records that were already parsed',
+						}]
+					if failed:
+						message['notes'] += [{
+							'type': 'error',
+							'text': f'Failed to parse {skipped} unrecognised records',
+						}]
+				# TODO: More robust error dectection
+				except ValueError:
+					message = {
+						'type': 'error',
+						'text': f'Failed to parse file {record.name}: unrecognised format',
+					}
+				results['messages'] += [message]
+				# Write results to session, so that in case of failure or 
+				# timeout, the user is told what was parsed so far
+				request.session['results'] = results
+		else:
+			# Since we know what the possible errors are, show a more 
+			# user-friendly message instead of what's in `form.errors`.
+			results = {
+				'title': 'Error parsing records',
+				'messages': [{
+					'type': 'error',
+					'text': 'Invalid platform selected or no files selected for upload',
+				}]
+			}
+			request.session['results'] = results
+		return redirect('home')
+
+
+class HomeView(TemplateView):
+	template_name = 'home.html'
 	
 	def get_context_data(self, *args, **kwargs):
 		context = super().get_context_data(*args, **kwargs)
 		context.update({
-			'parsers': parsers,
 			'records': Record.objects.all(),
+			'results': self.request.session.pop('results', {}),
+			'additional': self.request.session.pop('additional', {}),
+			'parse_address_form': ParseAddressForm(),
+			'upload_records_form': UploadRecordsForm(),
 		})
 		return context
+
